0ad2ea57ecfc6b74c5edab39c035a1a99a2ccdc8
Omar Khamis <50887051+oakhamis@users.noreply.github.com>
2023-10-10 01:25:16 +0300

    Update Rcode_report.R

--- Rcode_report.R
+++ Rcode_report.R
@@ -1,3 +1,7 @@
+###### SEA LEVEL ANALYSIS WITH RESPECT TO GREENLAND & ANTARCTICA ICE SHEETS #####
+
+# Importing necessary libraries
+
 library("dplyr")
 
 library("jtools")
@@ -11,8 +15,6 @@ library("broom.mixed")
 library("summarytools")
 
 library("PerformanceAnalytics")
-
-###### SEA LEVEL ANALYSIS WITH RESPECT TO GREENLAND & ANTARCTICA ICE SHEETS #####
 
 ##### INPUTTING DATA INTO R & DATA PREPROCESSING #####
 
@@ -35,7 +37,7 @@ sea_level_year_mean <- Sea_level_columns %>% group_by(Year) %>% summarise_each(f
 
 #----------------------
 
-greenland_mass_columns <- subset.data.frame(greenland_mass, select = c(ï..Year.decimal,Greenland_mass_cumulative_loss))
+greenland_mass_columns <- subset.data.frame(greenland_mass, select = c(Ã¯..Year.decimal,Greenland_mass_cumulative_loss))
 
 colnames (greenland_mass_columns) <- c("Year", "Greenland_Mass_Cumulative_Loss")
 
@@ -45,7 +47,7 @@ greenland_mass_year_mean <-greenland_mass_columns %>% group_by(Year) %>% summari
 
 #----------------------
 
-antarctica_mass_columns <- subset.data.frame(antarctica_mass, select = c(ï..Year.decimal,Antarctica_mass_cumulative_loss))
+antarctica_mass_columns <- subset.data.frame(antarctica_mass, select = c(Ã¯..Year.decimal,Antarctica_mass_cumulative_loss))
 
 colnames (antarctica_mass_columns) <- c("Year", "Antarctica_Mass_Loss")
 
@@ -73,7 +75,7 @@ sealevel_greenland_03_19 <- inner_join(sea_level_year_mean_03_19,
 sealevel_greenland_antarctica_03_19 <- inner_join(sealevel_greenland_03_19, 
                                                   antarctica_mass_year_mean_03_19,
                                                   by = "Year")
-##### 1- Descriptive Statistics #####
+##### 1) Descriptive Statistics #####
 
 MyData_03_19 = sealevel_greenland_antarctica_03_19[,-2]
 
@@ -112,15 +114,15 @@ hist(Antarctica_Mass_Cumulative_Loss, main = "Antarctica_Mass_Cumulative_Loss", 
 
 boxplot(Antarctica_Mass_Cumulative_Loss, col = 5)
 
-##### 2- correlation #####
+##### 2) Correlation #####
 
 chart.Correlation(MyData_03_19[,-1], histogram=T, pch="+")
 
 #-------------------------------------------------------
 
-##### 3- Regression #####
+##### 3) Regression #####
 
-## A- simple regression 
+## A) simple regression 
 
 #----------------------
 
@@ -128,19 +130,19 @@ model1 = lm (GMSL_Cumulative_Rise~Greenland_Mass_Cumulative_Loss)
 
 summ(model1,confint = TRUE, digits = 3)
 
-# check normality (Shapiro-Wilk test)
+# Check normality (Shapiro-Wilk test)
 
 qqnorm(model1$residuals); qqline(model1$residuals,col=2)
 
 shapiro.test(model1$residuals)
 
-# check Equal variance / Homoscedasticity 
+# Check Equal variance / Homoscedasticity 
 
 plot(model1$fitted.values, model1$residuals )
 
-abline(0,0,  col = 3)
+abline (0,0,  col = 3)
 
-# check include "Antarctica_Mass_Cumulative_Loss" as an additional predictor. 
+# Check include "Antarctica_Mass_Cumulative_Loss" as an additional predictor. 
 
 plot(Antarctica_Mass_Cumulative_Loss, model1$residuals )
 
@@ -151,26 +153,26 @@ model2 = lm (GMSL_Cumulative_Rise~Antarctica_Mass_Cumulative_Loss)
 
 summ(model2)
 
-# check normality (Shapiro-Wilk test)
+# Check normality (Shapiro-Wilk test)
 
 qqnorm(model2$residuals); qqline(model2$residuals,col=2)
 
 shapiro.test(model2$residuals)
 
-# check Equal variance / Homoscedasticity 
+# Check Equal variance / Homoscedasticity 
 
 plot(model2$fitted.values, model2$residuals )
 
-abline(0,0,  col = 3)
+abline (0,0,  col = 3)
 
-  # check include "Greenland_Mass_loss" as an additional predictor. 
+# Check include "Greenland_Mass_loss" as an additional predictor. 
 
 plot(Greenland_Mass_Cumulative_Loss, model2$residuals )
 
 abline(0,0, col=2)
 
 
-## B- Multiple regression 
+## B) Multiple regression 
 
 #----------------------
 
@@ -184,65 +186,67 @@ qqnorm(model3$residuals); qqline(model3$residuals,col=2)
 
 shapiro.test(model3$residuals)
 
-# check Equal variance / Homoscedasticity 
+# Check Equal variance / Homoscedasticity 
 
 plot(model3$fitted.values, model3$residuals )
 
-abline(0,0,  col = 3)
+abline (0,0,  col = 3)
 
-car::vif(model3)  # since VIF > 10 , then we can not add the two variables in one model
+car::vif(model3)  # since VIF > 10, we can not add the two variables in one model
 
-#----- The final model
+### The final model
 
 ## We will suggest a new model based on a new variable:
 
-#   New variable = Sum of the two independent variables
+# New variable = Sum of the two independent variables
 
 SUM_Ice_Mass_Cumulative_Loss = Greenland_Mass_Cumulative_Loss + Antarctica_Mass_Cumulative_Loss
 
 model3 = lm (GMSL_Cumulative_Rise ~ SUM_Ice_Mass_Cumulative_Loss)
 
-cor(GMSL_Cumulative_Rise,SUM_Ice_Mass_Cumulative_Loss)
+cor (GMSL_Cumulative_Rise,SUM_Ice_Mass_Cumulative_Loss)
 
 summ(model3)
 
-# check normality (Shapiro-Wilk test)
+# Check normality (Shapiro-Wilk test)
 
 qqnorm(model3$residuals); qqline(model3$residuals,col=2)
 
 shapiro.test(model3$residuals)
 
-# check Equal variance / Homoscedasticity 
+# Check Equal variance / Homoscedasticity 
 
 plot(model3$fitted.values, model3$residuals )
 
-abline(0,0,  col = 3)
+abline (0,0,  col = 3)
 
 ## Comparing model coefficients visually 
 
-plot_summs(model1, model2,model3, scale = TRUE,
+plot_summs(model1, model2, model3, scale = TRUE,
            plot.distributions = TRUE)
 
 export_summs(model1, model2, model3, scale = TRUE)
 
-#-------- time series forecasting
+#-------------------------------------------------------
 
-# Returns best ARIMA model according to either BIC value.
+# Time-series forecasting
+
+# Returns the best ARIMA model according to either BIC value.
 
 # The "auto.arima" function conducts a search over possible model within the period
 
-# the order constraints provided based on the following reference:
+# The order constraints provided based on the following reference:
 
-# Hyndman, RJ and Khandakar, Y (2008) "Automatic time series forecasting: The forecast package for R", Journal of Statistical Software, 26(3).
+# Hyndman, RJ, and Khandakar, Y (2008) "Automatic time series forecasting: The forecast package for R", Journal of Statistical Software, 26(3).
 
 ts.fit= auto.arima(ts(sea_level_year_mean_93_19$GMSL_Cumulative_Rise, 1993, 2019), ic="bic")
 
 ts.fit
 
-# check the residuals
+# Check the residuals
 checkresiduals(ts.fit,lag=10)
 
-# forecast 4 years (from 2020 to 2023 )
+# forecast 1 years (from 2020 to 2021 )
 forecast(ts.fit,h=1)
 
 plot(forecast(ts.fit,h=1))
